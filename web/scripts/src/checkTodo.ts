f79759fa0ad9123526e9104c165abc0f69cec4bc
Russell McClellan <russell.mcclellan@gmail.com>
2024-09-02 18:52:05 -0400

    Fix check-todo

--- web/scripts/src/checkTodo.ts
+++ web/scripts/src/checkTodo.ts
@@ -13,13 +13,30 @@ const filter = async function* <T>(
   }
 };
 
-export const checkTodos = async (): Promise<boolean> =>
+export const checkTodos = async (): Promise<boolean> => {
+  // Fail early if we aren't in a git repo
+  try {
+    await $`git rev-parse --is-inside-work-tree`.quiet();
+  } catch {
+    console.error("Not in a git repo");
+    return false;
+  }
+
+  const relevantFiles = await Array.fromAsync(
+    filter(
+      $`git ls-files -oc`.lines(),
+      (f) => f.includes(".rs") || f.includes(".hbs"),
+    ),
+  );
+
+  if (relevantFiles.length === 0) {
+    console.error("No relevant files found in TODO checker");
+    return false;
+  }
+
   // Returns false if there are any files with '*.rs' in the name that contain the string 'TODO'
-  (
-    await $`grep TODO ${await Array.fromAsync(
-      filter($`git ls-files`.lines(), (f) => f.includes(".rs")),
-    )}`.nothrow()
-  ).exitCode != 0;
+  return (await $`grep TODO ${relevantFiles}`.nothrow()).exitCode != 0;
+};
 
 export const execute = async () => {
   failUnless(await checkTodos());
