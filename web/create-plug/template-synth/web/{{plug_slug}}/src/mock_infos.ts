fc02808f33cfdc297d68d602df6df6c010c7c391
Russell McClellan <russell.mcclellan@gmail.com>
2024-11-03 01:59:05 -0400

    more reasonable stub synth

--- web/create-plug/template-synth/web/{{plug_slug}}/src/mock_infos.ts
+++ web/create-plug/template-synth/web/{{plug_slug}}/src/mock_infos.ts
@@ -2,13 +2,6 @@ import { Info } from "@conformal/plugin";
 
 const infos = new Map<string, Info>(
   Object.entries({
-    bypass: {
-      title: "Bypass",
-      type_specific: {
-        t: "switch",
-        default: false,
-      },
-    },
     gain: {
       title: "Gain",
       type_specific: {
