b6b473db3d6f742ff12663ac03e37be011b9c24d
Russell McClellan <russell.mcclellan@gmail.com>
2025-04-22 01:52:25 -0400

    add app note 3 (#58)

--- web/docs/pages/app_notes/_meta.ts
+++ web/docs/pages/app_notes/_meta.ts
@@ -2,4 +2,6 @@ export default {
   index: "Intro",
   "1-exponential-scale": "1: Parameterized Exponential Scaling Functions",
   "2-bbd-lfo": "2: Modeling LFOs for BBD Chorus",
+  "3-derivation-blep-blamp":
+    "3: Quick Derivation of BLIT, BLEP, BLAMP polynomials",
 };
