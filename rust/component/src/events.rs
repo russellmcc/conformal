e9142978361a0e64c09d3623022bbc0755c2133b
Russell McClellan <russell.mcclellan@gmail.com>
2025-04-26 14:41:24 -0400

    move unit tests inline (#61)

--- rust/component/src/events.rs
+++ rust/component/src/events.rs
@@ -1,8 +1,5 @@
 //! Contains data structures representing _events_ sent to [`crate::synth::Synth`]s
 
-#[cfg(test)]
-mod tests;
-
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 enum NoteIDInternals {
     NoteIDWithID(i32),
@@ -219,5 +216,66 @@ impl<I: Iterator<Item = Event>> IntoIterator for Events<I> {
 
     fn into_iter(self) -> Self::IntoIter {
         self.events
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Data, Event, Events, NoteData, NoteID};
+
+    static EXAMPLE_NOTE: NoteData = NoteData {
+        id: NoteID::from_pitch(60),
+        pitch: 60,
+        velocity: 1.0,
+        tuning: 0.0,
+    };
+
+    #[test]
+    fn out_of_order_events_rejected() {
+        assert!(
+            Events::new(
+                (&[
+                    Event {
+                        sample_offset: 5,
+                        data: Data::NoteOn {
+                            data: EXAMPLE_NOTE.clone()
+                        }
+                    },
+                    Event {
+                        sample_offset: 4,
+                        data: Data::NoteOff {
+                            data: EXAMPLE_NOTE.clone()
+                        }
+                    }
+                ])
+                    .iter()
+                    .cloned(),
+                10
+            )
+            .is_none()
+        )
+    }
+
+    #[test]
+    fn out_of_bounds_events_rejected() {
+        assert!(
+            Events::new(
+                (&[Event {
+                    sample_offset: 50,
+                    data: Data::NoteOn {
+                        data: EXAMPLE_NOTE.clone()
+                    }
+                },])
+                    .iter()
+                    .cloned(),
+                10
+            )
+            .is_none()
+        )
+    }
+
+    #[test]
+    fn empty_events_accepted() {
+        assert!(Events::new((&[]).iter().cloned(), 10).is_some())
     }
 }
--- rust/vst-wrapper/src/view.rs
+++ rust/vst-wrapper/src/view.rs
@@ -8,10 +8,6 @@ use conformal_component::parameters;
 use conformal_core::parameters::store;
 use conformal_ui::{self, Size, Ui, raw_window_handle};
 
-// Only include tests in test config on macos
-#[cfg(all(test, target_os = "macos"))]
-mod tests;
-
 struct SharedStore<S>(rc::Rc<RefCell<S>>);
 
 impl<S> Clone for SharedStore<S> {
@@ -258,4 +254,105 @@ impl<S: store::Store + 'static> IPlugViewTrait for SharedView<S> {
 
 impl<S> Class for SharedView<S> {
     type Interfaces = (IPlugView,);
+}
+
+// Only include tests in test config on macos
+#[cfg(all(test, target_os = "macos"))]
+mod tests {
+    use vst3::Steinberg::IPlugViewTrait;
+
+    use super::create;
+    use conformal_component::parameters;
+    use conformal_core::parameters::store;
+    struct DummyStore;
+
+    impl store::Store for DummyStore {
+        fn get(&self, _unique_id: &str) -> Option<parameters::Value> {
+            None
+        }
+
+        fn set_listener(&mut self, _listener: std::rc::Weak<dyn store::Listener>) {}
+
+        fn set(
+            &mut self,
+            _unique_id: &str,
+            _value: parameters::Value,
+        ) -> Result<(), store::SetError> {
+            Ok(())
+        }
+
+        fn set_grabbed(
+            &mut self,
+            _unique_id: &str,
+            _grabbed: bool,
+        ) -> Result<(), store::SetGrabbedError> {
+            Ok(())
+        }
+
+        fn get_info(&self, _unique_id: &str) -> Option<parameters::Info> {
+            None
+        }
+
+        fn set_ui_state(&mut self, _state: &[u8]) {}
+
+        fn get_ui_state(&self) -> Vec<u8> {
+            vec![]
+        }
+    }
+
+    #[test]
+    fn nsview_platform_supported() {
+        let v = create(
+            DummyStore {},
+            "test".to_string(),
+            conformal_ui::Size {
+                width: 100,
+                height: 100,
+            },
+        );
+        let nsview = std::ffi::CString::new("NSView").unwrap();
+        unsafe {
+            assert_eq!(
+                v.isPlatformTypeSupported(nsview.as_ptr()),
+                vst3::Steinberg::kResultTrue
+            );
+        }
+    }
+
+    #[test]
+    fn bananas_platform_not_supported() {
+        let v = create(
+            DummyStore {},
+            "test".to_string(),
+            conformal_ui::Size {
+                width: 100,
+                height: 100,
+            },
+        );
+        // Maybe some day, we will support bananas...
+        let nsview = std::ffi::CString::new("Bananas").unwrap();
+        unsafe {
+            assert_eq!(
+                v.isPlatformTypeSupported(nsview.as_ptr()),
+                vst3::Steinberg::kResultFalse
+            );
+        }
+    }
+
+    #[test]
+    fn defends_against_null_parent() {
+        let v = create(
+            DummyStore {},
+            "test".to_string(),
+            conformal_ui::Size {
+                width: 100,
+                height: 100,
+            },
+        );
+        let nsview = std::ffi::CString::new("NSView").unwrap();
+        assert_ne!(
+            unsafe { v.attached(std::ptr::null_mut(), nsview.as_ptr()) },
+            vst3::Steinberg::kResultOk
+        );
+    }
 }
