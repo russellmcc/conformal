35c13517f5f48b8a376e914ff83c105f3cf1e0ea
Russell McClellan <russell.mcclellan@gmail.com>
2025-11-02 21:41:00 -0500

    allow clients to opt-out of ci checks

--- web/scripts/src/ci.ts
+++ web/scripts/src/ci.ts
@@ -1,8 +1,17 @@
+import { z } from "zod";
 import runShell from "./runShell";
 import { Command } from "@commander-js/extra-typings";
 
+const packageJsonSchema = z.object({
+  scripts: z.record(z.string(), z.string()),
+});
+
 export const execute = async () => {
   process.env.CI = "1";
+
+  const rootPackageJson = packageJsonSchema.parse(
+    await Bun.file("package.json").json(),
+  );
 
   const actions = [
     "check-format",
@@ -18,6 +27,14 @@ export const execute = async () => {
   ];
 
   for (const action of actions) {
+    // If the action is not available in root package.json, skip it.
+    // This acts as a way for client projects to opt-out of certain checks.
+    const actionName = typeof action === "string" ? action : action[0];
+
+    if (!actionName || !rootPackageJson.scripts[actionName]) {
+      continue;
+    }
+
     if (typeof action === "string") {
       await runShell(["bun", "run", action]);
     } else {
