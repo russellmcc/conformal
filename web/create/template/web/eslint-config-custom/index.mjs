bf6b0e1057a45adf3e6fbb5fa07e2d80aa3c2ff5
Russell McClellan <russell.mcclellan@gmail.com>
2025-11-09 16:24:36 -0500

    support es 9 in template (#85)

---
+++ web/create/template/web/eslint-config-custom/index.mjs
@@ -0,0 +1,81 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
+import preferArrowFunctions from "eslint-plugin-prefer-arrow-functions";
+
+export default defineConfig([
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    extends: [
+      ts.configs.recommended,
+      ts.configs.strictTypeChecked,
+      ts.configs.stylisticTypeChecked,
+    ],
+    rules: {
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        { allowNumber: true },
+      ],
+      "@typescript-eslint/no-unnecessary-condition": [
+        "error",
+        { allowConstantLoopConditions: true },
+      ],
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    },
+  },
+  react.configs.flat.recommended ?? [],
+  react.configs.flat["jsx-runtime"] ?? [],
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.recommended,
+  preferArrowFunctions.configs.all,
+  {
+    ignores: ["dist"],
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react-hooks/exhaustive-deps": "error",
+      "prefer-arrow-functions/prefer-arrow-functions": [
+        "error",
+        {
+          returnStyle: "implicit",
+        },
+      ],
+      "no-warning-comments": "error",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.browser,
+        __dirname: "readonly",
+      },
+      parserOptions: {
+        sourceType: "module",
+      },
+    },
+  },
+]);
--- web/docs/next.config.js
+++ web/docs/next.config.js
@@ -10,8 +10,8 @@ export default withNextra({
   output: "export",
   images: {
     loader: "custom",
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 128, 384],
+    deviceSizes: [1280, 3840],
   },
   transpilePackages: ["next-image-export-optimizer"],
   env: {
