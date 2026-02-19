58150bba12ee14fed938ba3e97796e27a0f70cb2
Russell McClellan <russell.mcclellan@gmail.com>
2026-02-18 23:36:15 -0500

    Windows installers (#141)

--- web/docs/pages/docs/guides/_meta.ts
+++ web/docs/pages/docs/guides/_meta.ts
@@ -2,4 +2,5 @@ export default {
   quickstart: "Quick Start (macOS)",
   "quickstart-win": "Quick Start (Windows)",
   "macos-installers": "Build macOS Installers",
+  "windows-installers": "Build Windows Installers",
 };
