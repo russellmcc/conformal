0711c0745e35cebc843f312df43cecd7aeb3c98c
Russell McClellan <russell.mcclellan@gmail.com>
2026-02-21 17:23:28 -0500

    css

--- web/docs/app/layout.tsx
+++ web/docs/app/layout.tsx
@@ -3,6 +3,7 @@ import { Footer, Layout, Navbar } from "nextra-theme-docs";
 import { Anchor, Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
+import "katex/dist/katex.min.css";
 import React from "react";
 import { Metadata } from "next";
 
