29a85c3e7ea9e86a8b13b66e3ed526041f101a55
Russell McClellan <russell.mcclellan@gmail.com>
2026-03-18 22:45:33 -0400

    auto bump create when we bump a dep

--- web/internal-scripts/src/cli.ts
+++ web/internal-scripts/src/cli.ts
@@ -7,6 +7,7 @@ import {
   addRustChangeCommand,
   addRustPrepareReleaseCommand,
 } from "./rustChange";
+import { addSyncTemplateVersionsCommand } from "./syncTemplateVersions";
 
 export const command = () => {
   const command = new Command("conformal-internal-scripts").description(
@@ -19,6 +20,7 @@ export const command = () => {
   addDeployDocsCommand(command);
   addRustChangeCommand(command);
   addRustPrepareReleaseCommand(command);
+  addSyncTemplateVersionsCommand(command);
 
   return command;
 };
